a3704a14a69eb0b90063a0db486041b2cf478fb6
Ivan Kmotorka <xkmotorka@ivan.gmail.com>
2017-01-21 14:13:19 +0100

    update language

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ var middleware = {
 app.use(middleware.logger);
 
 app.get('/about',middleware.requireAuthentication , function(req, res){
-    res.send('hello express');
+    res.send('hello express!');
 });
 
 app.use(express.static(__dirname + '/public'));
